63cccc7d523369093f7468909b6760cf0ca894bf
Steven Loria <sloria1@gmail.com>
2016-06-01 21:39:57 -0400

    Add tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,1 +1,61 @@
+# -*- coding: utf-8 -*-
+import pytest
+from flask import Flask
+from flask.cli import ScriptInfo
+from click.testing import CliRunner
 
+from flask_konch.cli import cli
+
+##### Fixtures #####
+
+@pytest.fixture
+def make_app():
+    def maker(config):
+        app = Flask('testapp')
+        app.config['KONCH_SHELL'] = 'py'
+        app.config.update(config)
+        return app
+    return maker
+
+@pytest.fixture(scope='session')
+def runner():
+    return CliRunner()
+
+@pytest.fixture
+def run_command(runner, make_app):
+    def run(args=None, config=None):
+        app = make_app(config=config or {})
+        obj = ScriptInfo(create_app=lambda info: app)
+        return runner.invoke(cli, args or [], obj=obj)
+    return run
+
+##### Tests #####
+
+def test_no_args(run_command):
+    result = run_command()
+    assert result.exit_code == 0
+    # Flask imports
+    assert 'Blueprint, Config, Flask,' in result.output
+
+def test_no_flask_imports(run_command):
+    result = run_command(config={'KONCH_FLASK_IMPORTS': False})
+    assert 'Blueprint, Config, Flask,' not in result.output
+
+def test_additional_context(run_command):
+    result = run_command(config={'KONCH_CONTEXT': {'foo': 42, 'bar': 24}})
+    assert 'Additional variables:' in result.output
+    assert 'bar, foo' in result.output
+
+def test_banner(run_command):
+    result = run_command(config={'KONCH_BANNER': 'foobarbaz'})
+    assert 'foobarbaz' in result.output
+
+def test_prompt(run_command):
+    result = run_command(config={'KONCH_PROMPT': 'foo>>>>'})
+    assert 'foo>>>>' in result.output
+
+def test_context_format(run_command):
+    def format_ctx(ctx):
+        return 'foobarbaz'
+    result = run_command(config={'KONCH_CONTEXT_FORMAT': format_ctx})
+    assert 'foobarbaz' in result.output
